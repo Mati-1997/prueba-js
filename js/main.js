cc09dd7dc2efabe8fc33ae25239b6dd4ca5dd77b
Matias Fazio <matias.e.fazio@gmail.com>
2022-04-16 16:07:40 -0300

    Prueba js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//Esto es un comentario
+/* Comentario en varias lineas */
+
+//ES6
+let numeroUno;
+
+numeroUno = 1;
+numeroUno = "56";
+let nombre = "Maxi";
+
+const PI = 3.141516;
+
+let nombreUsuario = prompt("ingresa tu nombre");
+
+/* alert("hola " + nombreUsuario); */
+
+console.log("Hola " + nombreUsuario);
+console.log(23 + 54);
+
+let codigo = prompt("Ingresa tu codigo");
+
+let carnet =
+  "codigo: " +
+  codigo +
+  " " +
+  "Nombre: " +
+  persona +
+  " " +
+  "Pais" +
+  pais +
+  " " +
+  "Nacimiento " +
+  nacimiento +
+  " " +
+  "Domicilio: " +
+  domicilio;
+
+console.log(carnet);
